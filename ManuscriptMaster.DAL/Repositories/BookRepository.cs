f41395500d14b39b4e189d73c3126701fd4bd07a
Geckonda <Aquariusstd@yandex.ru>
2024-07-30 10:46:05 +0400

    added empty BookRepository

---
+++ ManuscriptMaster.DAL/Repositories/BookRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManuscriptMaster.DAL.Repositories
+{
+	internal class BookRepository
+	{
+	}
+}
